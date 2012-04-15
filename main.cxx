ca9b78e6cd5cdc090c1e53f36598cd6701d9f8db
Victor Smith <vgskb4@gmail.com>
2012-04-14 23:42:31 -0500

    Contains intital Working Copy

--- main.cxx
+++ main.cxx
@@ -1,6 +1,20 @@
 #include <iostream>
+using std::cout;
+using std::endl;
+typedef long long unsigned int myInt;
+
+const unsigned int maxValue=100;
+const unsigned int minValue= 1;
 
 int main(){
-
+	myInt SumSquares=0;
+	myInt SquaredSum=0;
+	for(unsigned int i= minValue; i<=maxValue; ++i)
+	{
+		SumSquares+=i*i;
+		SquaredSum+=i;
+	}
+	SquaredSum*=SquaredSum;
+	cout << SquaredSum - SumSquares<< endl;
 	return 0;
 }
