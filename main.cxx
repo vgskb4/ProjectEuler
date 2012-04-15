b7aa93f443df1aa76954b15ced4564f43eddfeea
Victor Smith <vgskb4@gmail.com>
2012-04-15 12:38:05 -0500

    initial working copy

--- main.cxx
+++ main.cxx
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <cmath>
+using std::sqrt;
+using std::cout;
+using std::endl;
 
-int main(){
 
+void testNumbers(const unsigned int &a,const unsigned int &b, unsigned int &c);
+const unsigned int RESULT_REQUIRED =1000;
+int main()
+{
+	bool found =false;
+	unsigned int c;
+	for(unsigned int a =1; !found; ++a){
+		for (unsigned int b=a+1; b < RESULT_REQUIRED; ++b){
+			testNumbers(a,b,c);
+			if (c!=0){
+				found =true;
+				cout << "a: " << a << " b: " << b << " c: " << c << endl;
+				cout << "The product results in : " << a*b*c << endl;
+				break;
+			}
+		}
+	}
 	return 0;
+}
+
+void testNumbers(const unsigned int &a, const unsigned int &b, unsigned int &c)
+{
+	double d=sqrt(a*a+b*b);
+	c=static_cast<int> (d);
+	if(a+b+c != RESULT_REQUIRED){
+		c=0;
+	}
+	if(c!=d){
+		c=0;
+	}
+	return;
 }
